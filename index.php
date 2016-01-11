e5441a776cf976c37db37d3435627dc1d66f1bad
joaosaragossa <joaosaragossa.dev@gmail.com>
2016-01-11 14:23:39 -0200

    Protótipo Home

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>OFICINA DE MARCENARIA</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header class="site header">
+      <h1 class="hidden">Oficina de Marcenaria</h1>
+      <img class="logo" src="img/logox250.png" alt="Logo">
+      <nav>
+        <h1 class="hidden">Menu</h1>
+        <ul>
+          <li><a href="#">Oficina</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Detalhes</a></li>
+          <li><a href="#">Registros</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="main">
+      <img class="imagem-home" src="img/marcenaria01.png" alt="Imagem da marcenaria">
+    </main>
+    <footer class="footer">
+      <div class="wrapper">
+        <p>Rua das Bananas 43 (12) 38323381 CEP 11680 000</p>
+        <p class="lang"><span><a href="#">PT</a></span><span><a href="#">IN</a></span></p>
+        <p><a href="mailto:contato@oficinademarcenaria.com.br">contato@oficinademarcenaria.com.br</a></p>
+      </div>
+    </footer>
+  </body>
+</html>
